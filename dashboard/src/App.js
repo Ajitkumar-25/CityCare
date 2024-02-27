c602d647fa85154e61ec2073e27ddeae54dd94f5
Ajitkumar-25 <kajit0408@gmail.com>
2024-02-27 23:32:19 +0530

    intialised folder structure

---
+++ dashboard/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidenav from './components/sidenav';
+
+function App() {
+  return (
+    <div>
+      <Sidenav />
+    </div>
+    
+  );
+}
+
+export default App;
