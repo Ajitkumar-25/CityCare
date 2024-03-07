2d692110375da18b3c57454db0ff8330461fcb35
Ajitkumar-25 <kajit0408@gmail.com>
2024-03-08 00:25:58 +0530

    added contact functionality

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,43 @@ const cors = require("cors");
 const port = 5000;
 app.use(express.json());
 app.use(cors());
+
+const nodemailer = require("nodemailer");
 const OrgModel = require("./Models/organization");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: process.env.SMTP_MAIL,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+app.post("/sendemail", (req, res) => {
+  const { email, subject, message } = req.body;
+  console.log(email, subject, message);
+  res.json("success");
+
+  const mailData = {
+    from: process.env.SMTP_MAIL,
+    to: email,
+    subject: subject,
+    text: message,
+  };
+
+  transporter.sendMail(mailData, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+});
+
 
 app.post("/register", (req, res) => {
   OrgModel.create(req.body)
@@ -28,6 +64,8 @@ app.post("/login", (req, res) => {
     }
   });
 });
+
+
 
 app.listen(port, () => {
   console.log(`Notes_App listening on port ${port}`);
